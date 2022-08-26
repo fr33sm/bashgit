d38bb420edb340bcb986184f6600ff6d8cb2343e
fr33sm <fr33sm@ya.ru>
2022-08-26 21:49:32 +0000

    made till -d

--- test.sh
+++ test.sh
@@ -36,20 +36,70 @@ while [ "$1" != "" ]; do
         fi
         ;;
     -c|--cpu) # работа с процессором
-        
-        # current load
-        top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "cpu current load: " 100 - $1"%"}'
-        # number of cores
-        ### cat /proc/cpuinfo | grep processor | wc -l
-        # stresstest CPU
-        ### fulload() { for ((i=1; i<=`nproc --all`; i++)); do while : ; do : ; done & done }; fulload; read; killall bash # get PID of process & kill only them
+        case $2 in
+            load) 
+                top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print "cpu current load: " 100 - $1"%"}'
+                ;;
+            temp)
+                if [[ -f "/sys/class/thermal/thermal_zone*/temp" ]]
+                then
+                    paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
+                fi
+                ;;
+            quantity)
+                cat /proc/cpuinfo | grep processor | wc -l
+                ;;
+            # stress)
+            #     fulload() { for ((i=1; i<=`nproc --all`; i++)); do while : ; do : ; done & done }; fulload; read; killall bash ### лучше сразу брать PID процессов и кикать их отдельно
+            #     ;;
+            *)
+                echo "используйте аругмент 'load' для отображения текущей нагрузки"
+                echo "используйте аргумент 'temp' для отображения текущей температуры"
+                echo "используйте аргумент 'quantity' для отображения количества ядер"
+                # echo "используйте аргумент 'stress' для стресс теста всех ядер процессора"
+                ;;
+        esac
+        shift
         ;;
     -m|--memory) # работа с памятью
-        cat /proc/meminfo | grep MemFree | awk -F " " '{printf "free memory: " $2/1024 " MB\n"}'
-        cat /proc/meminfo | grep MemAvailable | awk -F " " '{printf "available memory: " $2/1024 " MB\n"}'
+        case $2 in
+            total) 
+                cat /proc/meminfo | grep MemTotal | awk -F " " '{printf "total memory: " $2/1024 " MB\n"}'
+                ;;
+            available)
+                cat /proc/meminfo | grep MemAvailable | awk -F " " '{printf "available memory: " $2/1024 " MB\n"}'
+                ;;
+            swapinfo)
+                cat /proc/meminfo | grep SwapTotal | awk -F " " '{printf "total swap: " $2/1024 " MB\n"}'
+                cat /proc/meminfo | grep SwapFree | awk -F " " '{printf "free swap: " $2/1024 " MB\n"}'                
+                ;;
+            *)
+                echo "используйте аругмент 'total' для отображения общего объёма ОЗУ"
+                echo "используйте аргумент 'available' для отображения доступного к использованию объёма ОЗУ"
+                echo "используйте аргумент 'swapinfo' для отображения информации о SWAP"
+                ;;
+        esac
+        shift
         ;;
     -d|--disks) # работа с дисками
-        cat /proc/partitions | grep sda | wc -l | awk '{printf "number of disks: " $1 "\n"}'
+        case $2 in
+            quantity) 
+                lsblk | grep part | wc -l | awk '{printf "number of partitions: " $1 "\n"}'
+                ;;
+            list)
+                lsblk | grep sd
+                ;;
+            available)
+                df -Th | grep $3 | awk '{printf "available space: " $5 "\n"}'
+                shift
+                ;;
+            *)
+                echo "используйте аругмент 'quantity' для отображения общего количества разделов дисков"
+                echo "используйте 'list' для отображения списка дисков sd? (sda, sdb ...)"
+                echo "используйте аргумент 'available *имя диска*' для отображения доступного к использованию объёма ОЗУ"
+                ;;
+        esac
+        shift
         ;;
     -n|--network) # работа с сетью
         echo "network"
@@ -61,16 +111,17 @@ while [ "$1" != "" ]; do
         echo "$1"
         ;;
     -o|--output) # сохранение результатов работы скрипта на диск
-		echo "output path: $2"
+        if [[ -f "$2" ]]
+        then
+            echo "output path $2"
+        else
+            echo "output path not exist"
+        fi
 		shift
 		;;
     -t|--time) # текущее время
         echo "$1"
         ;;
-    # -te|--temperature) # температура с доступных датчиков
-    # 	paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/'
-    # 	smartctl -A /dev/sda
-    #	;;
     -h|--help)
         usage
         ;;
@@ -82,3 +133,5 @@ while [ "$1" != "" ]; do
 	esac
     shift
 done
+
+### output!!!
